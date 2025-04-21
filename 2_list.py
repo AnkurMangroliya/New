98f2cc2adccb3fa57f742c5a1fed7e130acf7246
AnkurMangroliya <ankurmangroliya099@gmail.com>
2025-04-20 23:20:35 -0400

    list added

---
+++ 2_list.py
@@ -0,0 +1,9 @@
+a = [1,2,3,4,5]
+print(a)
+print( 1 in a)
+
+# a[-1] will return last value, however begins with 0
+
+empty = []
+for i in empty:
+    print("Nothing will be printed because it's empty error.")
