3bb0eaa36fc8aead1d0726bce967b492d33fb8da
AnkurMangroliya <ankurmangroliya099@gmail.com>
2025-05-01 14:26:10 -0400

    Is_palindrome..

--- 100_leetcode_pelindrome.py
+++ 100_leetcode_pelindrome.py
@@ -8,3 +8,18 @@ class Solution:
     
 s = Solution()
 print(s.isPalindrome(23432))
+
+class Ram:
+    def ispali(self,x: int) -> bool:
+        if x<0 or (x%10==0 and x != 0):
+            return False
+        reversed_half=0
+
+        while reversed_half < x:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
+        
+        return x==reversed_half or x == reversed_half // 10
+
+r = Ram()
+print(r.ispali(123454321))
