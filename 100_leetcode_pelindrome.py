293d519a87febf1326e800ad47fac225d93d7c42
AnkurMangroliya <ankurmangroliya099@gmail.com>
2025-05-03 23:42:14 -0400

    Updated code..

--- 100_leetcode_pelindrome.py
+++ 100_leetcode_pelindrome.py
@@ -23,3 +23,17 @@ class Ram:
 
 r = Ram()
 print(r.ispali(123454321))
+
+
+# Best solution
+
+def final(s):
+    left,right = 0, len(s) - 1
+    while left<right:
+        if s[left] != s[right:]:
+            return False
+        left += 1
+        right -= 1
+    return True
+
+print(final('12321'))
