e374d211abcf3ede50549280434a9aad19bdd6c3
AnkurMangroliya <ankurmangroliya099@gmail.com>
2025-07-30 18:17:13 -0400

    numpy and pandas learning

--- learning test/new.py
+++ learning test/new.py
@@ -1,6 +1,23 @@
-# test_math.py
-def add(a, b):
-    return a + b
+# This is try and except example
+try:
+    result = 10 / 4
+    print(f"Result: {result}")
+except ZeroDivisionError as e:
+    print(f"An error occured: {e}")
+#  else will run if no exception occurs
+else:
+    print("No exception occurred, result is:", result)
+finally:
+    print("this must run after above cases.")
 
-def test_add():
-    assert add(2, 3) == 5
+
+# Exception and base Exception
+try:
+    result = 10 / 2
+except Exception as e:
+    print(e)
+except BaseException as e:
+    print("BaseException caught:", e)
+
+
+
