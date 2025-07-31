cacb560b159b85c984b33b74d888ef565e23c446
AnkurMangroliya <ankurmangroliya099@gmail.com>
2025-07-31 13:31:24 -0400

    Added linked list

--- Linked_list/basic.py
+++ Linked_list/basic.py
@@ -1,3 +1,8 @@
 # Linked list is a form of a sequence collection and it does not have to be in orer.
 # A linked list is made up of nodes that contain data and a reference to the next node.
 
+# Types of Linked List:
+# 1. Singly Linked List: Each node points to the next node and the last node points to None.
+# 2. Doubly Linked List: Each node points to both the next and previous nodes.
+# 3. Circular Linked List: The last node points back to the first node, forming a circle.   
+# 4. Circular Doubly Linked List: Combines the features of both circular and doubly linked lists.
